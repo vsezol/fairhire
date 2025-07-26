f688d254234d03a53b37767c3ccd229c7c5a0229
Vsevolod Zolotov <vsezold@gmail.com>
2025-07-26 12:38:54 +0300

    init

--- apps/tracker-electron/jest.config.ts
+++ apps/tracker-electron/jest.config.ts
@@ -10,11 +10,8 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@anticheat-interview/api-e2e',
+  displayName: '@anticheat-interview/tracker-electron',
   preset: '../../jest.preset.js',
-  globalSetup: '<rootDir>/src/support/global-setup.ts',
-  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
-  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
